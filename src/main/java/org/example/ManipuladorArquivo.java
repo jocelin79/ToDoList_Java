ae9edf2e77e9ce6e7864c1e17a9fa76cfbd3ccd4
Jocelin Nunes <91573405+jocelin79@users.noreply.github.com>
2022-10-14 14:24:35 -0300

    Update

--- src/main/java/org/example/ManipuladorArquivo.java
+++ src/main/java/org/example/ManipuladorArquivo.java
@@ -1,5 +1,30 @@
 package org.example;
 
-public class ManipuladorArquivo {
+import java.io.BefferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
 
+public class ManipuladorArquivo {
+  
+  public static void leitor(String path) throws IOException {
+    BufferedReader buffRead = new BufferedReader(new FileReader(path));
+    String linha = "";
+    while (true) {
+      if (linha != null) {
+        System.out.println(linha);
+      } else break;
+      linha = buffRead.readLne();
+    }
+    buffRead.close();
+  }
+  
+  public static void escritor(String path) throws IOException {
+    BufferedWriter buffWrite = new BufferedWriter (new FileWriter(path));
+    String linha = "";
+    For ()
+    linha = (objeto.toString);
+    buffWrite.append(linha + "\n");
+    buffWrite.close();
+  }
 }
