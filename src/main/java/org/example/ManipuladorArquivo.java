ae15499aee7938f8090abe17a8a0fc9b87da7616
jocelin79 <jocelinnunes@gmail.com>
2022-10-15 11:18:29 -0300

    Refatoração 3.0 para o Desafio K1-T3.

--- src/main/java/org/example/ManipuladorArquivo.java
+++ src/main/java/org/example/ManipuladorArquivo.java
@@ -1,31 +1,50 @@
 package org.example;
 
-import java.io.BefferedReader;
-import java.io.BufferedWriter;
-import java.io.FileReader;
-import java.io.FileWriter;
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class ManipuladorArquivo {
   
   public static void leitor(String path) throws IOException {
-    BufferedReader buffRead = new BufferedReader(new FileReader(path));
+
+    File file = new File(path);
+    if (!file.exists()) {
+      System.out.println("O arquivo, indicado pelo path, não existe.");
+      return;
+    }
+
+    BufferedReader buffRead = new BufferedReader(new FileReader(file));
     String linha = "";
-    List <String> taskList = new ArrayList<>(); 
+    List<String> taskList = new ArrayList<>();
     while (true) {
       if (linha != null) {
         taskList.add(linha);
       } else break;
-      linha = buffRead.readLne();
+      linha = buffRead.readLine();
     }
     buffRead.close();
+
+    //return linha;
   }
   
   public static void escritor(String path) throws IOException {
-    BufferedWriter buffWrite = new BufferedWriter (new FileWriter(path));
+
+    File file = new File(path);
+    try {
+      boolean isCreated = file.createNewFile();
+    } catch (IOException exception) {
+      exception.printStackTrace();
+      System.out.println("O arquivo, indicado pelo path, não existe e nem pôde ser criado.");
+    }
+
+    BufferedWriter buffWrite = new BufferedWriter (new FileWriter(file));
     String linha = "";
     For ()
     linha = (objeto.toString);
-    buffWrite.append(linha + "\n");
+    buffWrite.append(linha);
+    buffWrite.newLine();
+    buffWrite.flush();
     buffWrite.close();
   }
 }
